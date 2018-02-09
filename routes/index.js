701e93c299e2db751c5b932c91588e43e81b4321
nmastracci <mastracci.natalie@gmail.com>
2018-02-09 11:29:08 -0500

    iteration

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const templateTypes = [
+	{name: "Jade", usefulness: "sucky"},
+	{name: "EJS", usefulness: "better"},
+	{name: "Handlebars", usefulness: "Awesome"}
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', description: "better!!" });
+  res.render('index', { title: 'Express', description: "better!!", templates: templateTypes });
 });
 
 module.exports = router;
